f71711b85aee434a65ead4d8f6dd24913ab78e3e
alcoweb <alcowebdev@gmail.com>
2022-08-18 16:04:04 +0300

    added docker and Slim PHP

--- api/public/index.php
+++ api/public/index.php
@@ -1,11 +1,16 @@
 <?php
-    declare(strict_types = 1);
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-    header('Content-type: application/json');
+require __DIR__ . '/../vendor/autoload.php';
 
-    echo json_encode(
-        [
-            'name' => 'Test API',
-            'version' => '1.0'
-        ]
-    );
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $payload = json_encode(['hello' => 'world'], JSON_PRETTY_PRINT);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
